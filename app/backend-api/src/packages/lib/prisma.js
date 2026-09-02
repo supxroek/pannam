97f000c753f48d5059b495f9740477159b410f93
Suparoek Manajit <oaffanmanajit@gmail.com>
2026-09-03 01:43:26 +0700

    update: prisma

--- app/backend-api/src/packages/lib/prisma.js
+++ app/backend-api/src/packages/lib/prisma.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../generated/prisma/client.ts";
+import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from "../../generated/prisma/client.ts";
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
