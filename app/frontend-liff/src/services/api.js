e98e9ec7aaaaf6a885bab9dbdca4b10a771ff922
Suparoek Manajit <oaffanmanajit@gmail.com>
2026-09-03 17:40:21 +0700

    update: เปลี่ยนจาก localhost มาใช้ของ ngrok

--- app/frontend-liff/src/services/api.js
+++ app/frontend-liff/src/services/api.js
@@ -8,7 +8,7 @@
 const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL ||
   (import.meta.env.PROD
-    ? "http://100.75.191.71:3000" // Production: https://pannam-api.vercel.app
+    ? "https://fitting-allegedly-chicken.ngrok-free.app" // Production: https://pannam-api.vercel.app
     : "http://localhost:3000");
 
 /**
