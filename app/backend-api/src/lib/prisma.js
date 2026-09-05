82b7fab75dcea64e0a094c3cbd2214b5813eb977
Suparoek Manajit <oaffanmanajit@gmail.com>
2026-09-05 20:09:17 +0700

    update(backend): แก้ไขบั๊ค deploy error

--- app/backend-api/src/lib/prisma.js
+++ app/backend-api/src/lib/prisma.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from '@prisma/client/default.js';
+import { PrismaClient } from '@prisma/client';
 // import { PrismaClient } from "../../generated/prisma/client.ts";
 
 const adapter = new PrismaPg({
